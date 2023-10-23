75f566a2a7467946f27f93bed00199d6d38d30d0
Artem Vazerov <avazerov@inno.tech>
2023-10-24 01:54:28 +0300

    хотфикс

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,17 +1,18 @@
 import static java.lang.Math.floor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Formatter {
     String Formatter(double price) {
         int modPrice = (int) (floor(price));
-        int[] exceptNumbres = {111, 112, 113, 114};
-        for (int num : exceptNumbres) {
-            if (num == modPrice) {
-                return "рублей";
-            }
-        }
-        modPrice = modPrice % 10;
+        if (modPrice % 100 > 11 && modPrice % 100 < 15)
+            modPrice = modPrice % 100;
+        else if (modPrice > 11 && modPrice < 15)
+            return "рублей";
+        else
+            modPrice = modPrice % 10;
+
 // Это студия такой красивый формат(котлин стайл)
 // привела, сам не догадался =(
         return switch (modPrice) {
